f620697f8cd8310344adba3283bfffeef9494e72
Abhinav Dhiman <abhinavg916@gmail.com>
2020-09-03 23:02:09 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Search from "./components/search/Search";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Title from "./components/Title";
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
+          <Title />
           <Search />
         </div>
       </MuiThemeProvider>
